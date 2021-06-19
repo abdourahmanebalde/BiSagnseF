53fad88c8d63a2c7017e28636616c155743b999c
Malick DKT <mac@MacBook-Pro.local>
2021-06-19 22:00:45 +0000

    --- Auto-Git Commit ---

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';  
+declare var $: any
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  name = 'Jquery Integration With Angular!';  
+  isJqueryWorking: any;  
+  ngOnInit() {  
+    $(document).ready(() => {  
+        this.isJqueryWorking = 'Jquery is working !!!';  
+    });  
+  } 
   title = 'bisagnse-front';
 }
